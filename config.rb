f360dc14a0616683e6bfd8ed0d56b95551401723
Doug Yun <yun.douglas@gmail.com>
2013-09-14 18:51:43 -0400

    first post

--- config.rb
+++ config.rb
@@ -23,9 +23,8 @@ activate :sprockets
 Time.zone = 'US/Eastern'
 
 activate :blog do |blog|
-  blog.prefix = '/blog'
   blog.permalink = ':year/:month/:day/:title.html'
-  blog.sources = ':year-:month-:day-:title.html'
+  blog.sources = 'posts/:year-:month-:day-:title.html'
   blog.taglink = 'tags/:tag.html'
   blog.layout = 'article'
   blog.summary_separator = /(READMORE)/
---
+++ source/posts/2013-09-14-first-post.md
@@ -0,0 +1,8 @@
+---
+title: First Post
+date: 2013-09-14
+tags: 
+---
+
+Jeez, styling is a pain in the ass. Will be fooling around with the
+layout too... this is good for now.
